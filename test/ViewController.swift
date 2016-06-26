d94f47fe7d6b51a1b15384505ed1a0b820308d57
lixiaojin123 <20141105062l@FanydeiMac13.local>
2016-06-26 21:16:12 +0800

    Phone计算器第二次
    首先打开Xcode，创建一个项目，点击iOS，选择Application再选择Single view
    Application,把型号设置为iPhone，添加背景色和字的颜色。添加一个关系为outlet和若干个关系为action的按钮，并给每个按钮
    起名

---
+++ test/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by 李晓晋 on 16/6/22.
+//  Copyright © 2016年 李晓晋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var table: UITextField!
+    
+    @IBAction func zero(sender: AnyObject) {
+    }
+    @IBAction func one(sender: AnyObject) {
+    }
+    @IBAction func two(sender: AnyObject) {
+    }
+    @IBAction func three(sender: AnyObject) {
+    }
+    @IBAction func four(sender: AnyObject) {
+    }
+    @IBAction func five(sender: AnyObject) {
+    }
+    
+    @IBAction func six(sender: AnyObject) {
+    }
+    @IBAction func seven(sender: AnyObject) {
+    }
+   
+    @IBAction func eight(sender: AnyObject) {
+    }
+    @IBAction func nine(sender: AnyObject) {
+    }
+    @IBAction func point(sender: AnyObject) {
+    }
+    @IBAction func add(sender: AnyObject) {
+    }
+    
+    @IBAction func subract(sender: AnyObject) {
+    }
+    @IBAction func ride(sender: AnyObject) {
+    }
+    @IBAction func divide(sender: AnyObject) {
+    }
+    
+    @IBAction func percent(sender: AnyObject) {
+    }
+    @IBAction func exchange(sender: AnyObject) {
+    }
+    @IBAction func clean(sender: AnyObject) {
+    }
+    
+    @IBAction func equal(sender: AnyObject) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
