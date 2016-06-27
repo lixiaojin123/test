ce2e6f3d64cecce814d45c353e82c776cd9d45e2
lixiaojin123 <20141105062l@FanydeiMac13.local>
2016-06-27 13:03:22 +0800

    Phone计算器第三次
    iPhone计算器第三次：定义一个变量flag，根据flag的值进行相应的运算，在定义一个临时变量temp，为浮点数。对于数0；如果文本中有0，
    则不再加任何内容。否则在文本中添加0，对于数1-－9，如果文本中有0，则将文本赋为空并加相应的数，否则直接在文本中加1。对于加减乘除，将开始按下
    的值写入临时变量temp中，将文本置为空通过按下以后给flag赋值。取相反数的按键是将输入的值放在temp中。然后取相反数输出。＝按钮是利用sw
    itch
    case选择语句，根据flag的值选择不同的算法。将后来输入的数强制转换为double型然后和temp相加赋给temp，输出temp，除法运算的
    先判断分母是否为0。

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -9,62 +9,254 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var table: UITextField!
-    
-    @IBAction func zero(sender: AnyObject) {
-    }
-    @IBAction func one(sender: AnyObject) {
-    }
-    @IBAction func two(sender: AnyObject) {
-    }
-    @IBAction func three(sender: AnyObject) {
-    }
-    @IBAction func four(sender: AnyObject) {
-    }
-    @IBAction func five(sender: AnyObject) {
-    }
-    
-    @IBAction func six(sender: AnyObject) {
-    }
-    @IBAction func seven(sender: AnyObject) {
-    }
-   
-    @IBAction func eight(sender: AnyObject) {
-    }
-    @IBAction func nine(sender: AnyObject) {
-    }
-    @IBAction func point(sender: AnyObject) {
-    }
-    @IBAction func add(sender: AnyObject) {
-    }
-    
-    @IBAction func subract(sender: AnyObject) {
-    }
-    @IBAction func ride(sender: AnyObject) {
-    }
-    @IBAction func divide(sender: AnyObject) {
-    }
-    
-    @IBAction func percent(sender: AnyObject) {
-    }
-    @IBAction func exchange(sender: AnyObject) {
-    }
-    @IBAction func clean(sender: AnyObject) {
-    }
-    
-    @IBAction func equal(sender: AnyObject) {
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBOutlet weak var Text: UITextField!
+    var flag:Int=0;
+    var temp:Double=0.0;
+    @IBAction func zero(sender: AnyObject) {
+        if(Text.text=="0")
+        {
+            Text.text=(Text.text)!+"";
+        }
+        else
+        {
+            Text.text=(Text.text)!+"0";
+        }
 
+        
+    }
+    @IBAction func one(sender: AnyObject) {
+        if( Text.text=="0")
+        {
+            Text.text=""
+            Text.text=(Text.text)!+"1"
+            
+        }
+        else
+        {
+            Text.text=(Text.text)!+"1";
+        }
+    }
+    @IBAction func two(sender: AnyObject) {
+        if( Text.text=="0")
+        {
+            Text.text=""
+            Text.text=(Text.text)!+"2"
+            
+        }
+        else
+        {
+            Text.text=(Text.text)!+"2";
+        }
+    }
+    @IBAction func three(sender: AnyObject) {
+        if( Text.text=="0")
+        {
+            Text.text=""
+            Text.text=(Text.text)!+"3"
+            
+        }
+        else
+        {
+            Text.text=(Text.text)!+"3";
+        }
+    }
+    @IBAction func four(sender: AnyObject) {
+        if( Text.text=="0")
+        {
+            Text.text=""
+            Text.text=(Text.text)!+"4"
+            
+        }
+        else
+        {
+            Text.text=(Text.text)!+"4";
+        }
+    }
+    @IBAction func five(sender: AnyObject) {
+        if( Text.text=="0")
+        {
+            Text.text=""
+            Text.text=(Text.text)!+"5"
+            
+        }
+        else
+        {
+            Text.text=(Text.text)!+"5";
+        }
+    }
+    
+    @IBAction func six(sender: AnyObject) {
+        if( Text.text=="0")
+        {
+            Text.text=""
+            Text.text=(Text.text)!+"6"
+            
+        }
+        else
+        {
+            Text.text=(Text.text)!+"6";
+        }
+    }
+    @IBAction func seven(sender: AnyObject) {
+        if( Text.text=="0")
+        {
+            Text.text=""
+            Text.text=(Text.text)!+"7"
+            
+        }
+        else
+        {
+            Text.text=(Text.text)!+"7";
+        }
+    }
+   
+    @IBAction func eight(sender: AnyObject) {
+        if( Text.text=="0")
+        {
+            Text.text=""
+            Text.text=(Text.text)!+"8"
+            
+        }
+        else
+        {
+            Text.text=(Text.text)!+"8";
+        }
+    }
+    @IBAction func nine(sender: AnyObject) {
+        if( Text.text=="0")
+        {
+            Text.text=""
+            Text.text=(Text.text)!+"9"
+            
+        }
+        else
+        {
+            Text.text=(Text.text)!+"9";
+        }
+    }
+    @IBAction func point(sender: AnyObject) {
+        if( Text.text=="0")
+        {
+            Text.text=""
+            Text.text=(Text.text)!+"."
+            
+        }
+        else
+        {
+            Text.text=(Text.text)!+".";
+        }
+    }
+    @IBAction func add(sender: AnyObject) {
+        if flag==0{
+            temp=(Double)(Text.text!)!
+            Text.text=""
+        }
+        flag=1
+    }
+    
+    @IBAction func subract(sender: AnyObject) {
+        if flag==0{
+            temp=(Double)(Text.text!)!
+            Text.text=""
+        }
+        flag=2
 
+    }
+    @IBAction func ride(sender: AnyObject) {
+        if flag==0{
+            temp=(Double)(Text.text!)!
+            Text.text=""
+        }
+        flag=3
+
+    }
+    @IBAction func divide(sender: AnyObject) {
+        if flag==0{
+            temp=(Double)(Text.text!)!
+            Text.text=""
+        }
+        flag=4
+
+    }
+    
+    @IBAction func percent(sender: AnyObject) {
+        if  flag==1||flag==2||flag==3||flag==4{
+            switch  flag{
+            case 1:
+                temp=temp+(Double)(Text.text!)!
+                temp=temp/100
+                Text.text="\(temp)"
+            case 2:
+                temp=temp-(Double)(Text.text!)!
+                temp=temp/100
+                Text.text="\(temp)"
+            case 3:
+                temp=temp*(Double)(Text.text!)!
+                temp=temp/100
+                Text.text="\(temp)"
+            case 4:
+                if(Double)(Text.text!)!==0
+                {
+                    Text.text="o不能为除数"
+                }
+                else{
+                    temp=temp/(Double)(Text.text!)!
+                    temp=temp/100
+                    Text.text="\(temp)"
+                }
+            default:
+                break
+            }
+        }
+        else{
+        temp=(Double)(Text.text!)!
+        temp=temp/100
+        Text.text="\(temp)"}
+
+    }
+    @IBAction func exchange(sender: AnyObject) {
+        temp=(Double)(Text.text!)!
+        temp=(-temp)
+        Text.text="\(temp)"
+    }
+    @IBAction func clean(sender: AnyObject) {
+    }
+    
+    @IBAction func equal(sender: AnyObject) {
+        switch  flag{
+        case 1:
+            temp=temp+(Double)(Text.text!)!
+            Text.text="\(temp)"
+        case 2:
+            temp=temp-(Double)(Text.text!)!
+            Text.text="\(temp)"
+        case 3:
+            temp=temp*(Double)(Text.text!)!
+            Text.text="\(temp)"
+        case 4:
+            if(Double)(Text.text!)!==0
+            {
+                Text.text="o不能为除数"
+            }
+            else{
+                temp=temp/(Double)(Text.text!)!
+                Text.text="\(temp)"
+            }
+        default:
+            break
+        }
+    }
+    
 }
 
